0decefd7e8125f4ab2e0e66f6515c4b40e3c04ca
Richard Nai <richardnai6@gmail.com>
2021-02-20 00:32:06 +0100

    Working with csv path and linearization path

--- OrdinaryLeastSquares.py
+++ OrdinaryLeastSquares.py
@@ -1,17 +1,38 @@
 import numpy as np
 from scipy import constants
+from typing import NamedTuple
 
 import pandas as pd
 
 
-def TrimData(data: pd.DataFrame, linearization_point: dict, data_constraints: dict):
+class LinearizationPoint(NamedTuple):
+    u: float = 0
+    w: float = 0
+    theta: float = 0
+    pitch_ctrl: float = 0
+    throttle_ctrl: float = 0
+
+class TrimmedData(NamedTuple):
+    u: pd.Series
+    w: pd.Series
+    q: pd.Series
+    theta: pd.Series
+    udot: pd.Series
+    wdot: pd.Series
+    qdot: pd.Series
+    pitch: pd.Series
+    throttle: pd.Series
+
+
+def TrimData(data: pd.DataFrame, linearization_point: LinearizationPoint, data_constraints: dict = None):
     """
     dataConstraints is a map of data key to maximum allowable deviation
     """
     if data_constraints is not None:
+        lp_dict = linearization_point._asdict()
         invalidList = np.zeros(len(data), dtype=bool)
         for key, val in data_constraints.items():
-            invalidList |= (abs(data[key] - linearization_point.get(key, 0)) >= val)
+            invalidList |= (abs(data[key] - lp_dict.get(key, 0)) >= val)
 
         # TODO there might be a more Pythonic way to do this
         toRemove = [0] * sum(invalidList)
@@ -21,69 +42,64 @@ def TrimData(data: pd.DataFrame, linearization_point: dict, data_constraints: di
             idx += 1
         data.drop(toRemove)
 
-    u = data['u'] - linearization_point['u']
-    w = data['w'] - linearization_point['w']
+    u = data['u'] - linearization_point.u
+    w = data['w'] - linearization_point.w
     q = data['q']
-    theta = data['theta'] - linearization_point['theta']
-    udot = data['du/dt']
-    wdot = data['dw/dt']
-    qdot = data['dq/dt']
-    pitch = data['pitch_ctrl'] - linearization_point['pitch_ctrl']
-    throttle = data['throttle_ctrl'] - linearization_point['throttle_ctrl']
+    theta = data['theta'] - linearization_point.theta
+    udot = data['u_dot']
+    wdot = data['w_dot']
+    qdot = data['q_dot']
+    pitch = data['pitch_ctrl'] - linearization_point.pitch_ctrl
+    throttle = data['throttle_ctrl'] - linearization_point.throttle_ctrl
 
-    return {'u': u, 'w': w, 'q': q, 'theta': theta, 'udot': udot, 'wdot': wdot, 'qdot': qdot, 'pitch': pitch,
-            'throttle': throttle}
+    return TrimmedData(u, w, q, theta, udot, wdot, qdot, pitch, throttle)
 
 
-def MMSE_all_free(u: pd.Series, w: pd.Series, q: pd.Series, theta: pd.Series, udot: pd.Series, wdot: pd.Series,
-                  qdot: pd.Series, pitch: pd.Series, throttle: pd.Series):
-    x = np.array([u, w, q, theta, pitch, throttle]).T
+def MMSE_all_free(data: TrimmedData):
+    x = np.array([data.u, data.w, data.q, data.theta, data.pitch, data.throttle]).T
     xTxi = x.T @ x
     xTxi = np.linalg.inv(xTxi)
 
-    X_coeffs = xTxi @ x.T @ udot
-    Z_coeffs = xTxi @ x.T @ wdot
-    M_coeffs = xTxi @ x.T @ qdot
+    X_coeffs = xTxi @ x.T @ data.udot
+    Z_coeffs = xTxi @ x.T @ data.wdot
+    M_coeffs = xTxi @ x.T @ data.qdot
 
     return [X_coeffs, Z_coeffs, M_coeffs]
 
 
-def MMSE_fixed_theta_1d_throttle(theta_ref: float, u: pd.Series, w: pd.Series, q: pd.Series, theta: pd.Series,
-                                 udot: pd.Series, wdot: pd.Series, qdot: pd.Series, pitch: pd.Series,
-                                 throttle: pd.Series):
-    x = np.array([u, w, q, pitch, throttle]).T
+def MMSE_fixed_theta_1d_throttle(theta_ref: float, data: TrimmedData):
+    x = np.array([data.u, data.w, data.q, data.pitch, data.throttle]).T
     xTxi = x.T @ x
     xTxi = np.linalg.inv(xTxi)
 
     Xtheta = -constants.g * np.cos(theta_ref)
-    u_minusg = udot - Xtheta * theta
+    u_minusg = data.udot - Xtheta * data.theta
     X_coeffs = xTxi @ x.T @ u_minusg
     X_coeffs = np.insert(X_coeffs, 3, Xtheta)
 
-    x = np.array([u, w, q, pitch]).T
+    x = np.array([data.u, data.w, data.q, data.pitch]).T
     xTxi = x.T @ x
     xTxi = np.linalg.inv(xTxi)
 
     Ztheta = -constants.g * np.sin(theta_ref)
-    w_minusg = wdot - Ztheta * theta
+    w_minusg = data.wdot - Ztheta * data.theta
     Z_coeffs = xTxi @ x.T @ w_minusg
     Z_coeffs = np.insert(Z_coeffs, 3, Ztheta)
     Z_coeffs = np.append(Z_coeffs, 0)
 
-    M_coeffs = xTxi @ x.T @ qdot
+    M_coeffs = xTxi @ x.T @ data.qdot
     M_coeffs = np.insert(M_coeffs, 3, 0)
     M_coeffs = np.append(M_coeffs, 0)
 
     return [X_coeffs, Z_coeffs, M_coeffs]
 
 
-def MSE(A: np.array, B: np.array, u: pd.Series, w: pd.Series, q: pd.Series, theta: pd.Series, udot: pd.Series,
-        wdot: pd.Series, qdot: pd.Series, pitch: pd.Series, throttle: pd.Series):
-    sys = np.array([u, w, q, theta])
-    ctrl = np.array([pitch, throttle])
+def MSE(A: np.array, B: np.array, data: TrimmedData):
+    sys = np.array([data.u, data.w, data.q, data.theta])
+    ctrl = np.array([data.pitch, data.throttle])
     estimated = A @ sys + B @ ctrl
-    actual = np.array([udot, wdot, qdot])
-    return ((estimated - actual) ** 2).sum(axis=1) / len(u)
+    actual = np.array([data.udot, data.wdot, data.qdot])
+    return ((estimated - actual) ** 2).sum(axis=1) / len(data.u)
 
 
 def printCoefs(X_coeffs: np.array, Z_coeffs: np.array, M_coeffs: np.array):
--- main.py
+++ main.py
@@ -1,50 +1,56 @@
 import pandas as pd
 import numpy as np
 import argparse
+import json
+from types import SimpleNamespace as Namespace
+import sys
+import os
 
 import OrdinaryLeastSquares as ols
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-csv_path', help='Path of CSV file')
-    parser.add_argument('--linearization', help='Path of Linearization defs')
-    parser.add_argument('--limits', help='Path of maximum deviation from linearization defs')
-    data = pd.read_csv('data/data.csv')
+    parser.add_argument('-generate_default_configs', action='store_true', help='Generate default config files')
+    parser.add_argument('-lin_path', help='Path of Linearization defs')
+    parser.add_argument('--limit_path', help='Path of maximum deviation from linearization defs')
 
-    u_ref = 55
-    w_ref = 0.390986
-    theta_ref = 0.0935541 / 180 * np.pi
+    args = parser.parse_args()
 
-    pitch_trim = -0.170525
-    throttle_trim = 0.534835
+    if args.generate_default_configs:
+        if not os.path.exists('config'):
+            os.mkdir('config')
 
-    trimmed_data = ols.TrimData(data, u_ref, w_ref, theta_ref, pitch_trim, throttle_trim)
+        f = open('config/default_linearization_point.json', 'w')
+        f.write(json.dumps(ols.LinearizationPoint()._asdict(),
+                           default=lambda o: o.__dict__, indent=4))
+        f.close()
 
-    X_coeffs, Z_coeffs, M_coeffs = ols.MMSE_fixed_theta_1d_throttle(theta_ref=theta_ref, **trimmed_data)
-    # print('X_coeffs:', X_coeffs)
-    # print('Z_coeffs:', Z_coeffs)
-    # print('M_coeffs:', M_coeffs)
+        sys.exit(0)
+
+    if args.csv_path is None or args.lin_path is None:
+        print("Either -csv_path or -generate_default_configs and -linearization_path must be specified")
+        sys.exit(1)
+
+    data = pd.read_csv(args.csv_path)
+    json_data = open(args.lin_path, "r").read()
+    linearization = json.loads(json_data, object_hook=lambda d: Namespace(**d))
+
+    limits = None
+    if args.limit_path is not None:
+        json_data = open(args.limit_path, "r").read()
+        limits = json.loads(json_data)
+
+    trimmed_data = ols.TrimData(data, linearization, limits)
+
+    X_coeffs, Z_coeffs, M_coeffs = ols.MMSE_fixed_theta_1d_throttle(linearization.theta, trimmed_data)
     A = np.array([X_coeffs[0:4], Z_coeffs[0:4], M_coeffs[0:4]])
     B = np.array([X_coeffs[4:6], Z_coeffs[4:6], M_coeffs[4:6]])
     print('A:')
     print(A)
     print('B:')
     print(B)
-    res = ols.MSE(A, B, **trimmed_data)
+    res = ols.MSE(A, B, trimmed_data)
     print('MSE residuals:', res)
 
     ols.printCoefs(X_coeffs, Z_coeffs, M_coeffs)
-
-
-def generate_config(params, file_path):
-    print("Saving Configs")
-    f = open(file_path, "w")
-    json_data = json.dumps(params.__dict__, default=lambda o: o.__dict__, indent=4)
-    f.write(json_data)
-    f.close()
-
-
-def read_config(config_path):
-    print('Parse Params file here from ', config_path, ' and pass into main')
-    json_data = open(config_path, "r").read()
-    return json.loads(json_data, object_hook=lambda d: Namespace(**d))
